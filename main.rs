f43d7c5cfab4854cdc4796092a2217b28c2352f3
szenimaru <51009633+szenimaru@users.noreply.github.com>
2020-11-21 19:33:50 +0900

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,45 @@
+extern crate ndarray;
+use ndarray::prelude::*;
+//[dependencies]
+//ndarray = { version = "0.13.0", features = ["blas"] }
+
+fn heat_1dim() {
+    let L = 1.0;
+    let M = 10;
+    
+    let dx = L/M as f64;//f64 float
+
+    let dt = 0.01;
+    let N = 500;
+
+    let alpha = 0.01;
+    let gamma = dt/(dx*dx) as f64;
+
+    let a = alpha*gamma;
+
+    let mut T = Array::<f64, _>::zeros(M);
+    for i in 0..M {
+        T[i] = 40.0;
+    }
+    println!("{}",T);
+
+    let mut T_n = Array::<f64, _>::zeros(M);//next T
+
+    
+    for step in 0..N {
+        for i in 0..M {
+            let mut T_b = if i == 0 { 10.0 } else { T[i-1] };
+            let mut T_f = if i == M-1 { T[M-1] } else { T[i+1] };
+            T_n[i] = a*T_f+(1.0-2.0*a)*T[i]+a*T_b;
+        }
+        T = T_n.clone();
+        if step%10 == 0 {
+            println!("{}",T);
+        }
+    }
+
+}
+
+fn main() {
+    heat_1dim();
+}
